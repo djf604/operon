4d394e17da8c885dcd2ddf5952e6975e8849d404
Dominic Fitzgerald <djf604@truman.edu>
2018-01-24 15:27:13 -0600

    Updated distribution files, prepping for upload to PyPI

--- setup.py
+++ setup.py
@@ -5,29 +5,32 @@ import operon
 setup(
     name='Operon',
     version=operon.__version__,
-    description='Pipeline design and distribution framework',
-    license='MIT',
+    description='Dataflow pipeline development framework, powered by Parsl',
+    license='GNU GPLv3',
     author='Dominic Fitzgerald',
     author_email='dominicfitzgerald11@gmail.com',
     url='https://github.com/djf604/operon',
     download_url='https://github.com/djf604/operon/tarball/{}'.format(operon.__version__),
     packages=find_packages(),
-    install_requires=['pathos>=0.2.1', 'six', 'libsubmit', 'parsl==0.3.1', 'networkx==2.0'],
+    install_requires=['libsubmit', 'parsl==0.3.1', 'networkx==2.0'],
     entry_points={
         'console_scripts': [
             'operon = operon._util:execute_from_command_line'
         ]
     },
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
         'Topic :: Software Development :: Libraries',
-        'License :: OSI Approved :: MIT License'
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
     ]
 )
