10c2d2f086633d591c119e0dd20e67e9b525e4cc
Dominic Fitzgerald <djf604@truman.edu>
2018-07-20 11:53:55 -0500

    The class used to define the pipeline can now be called whatever, so long as there's only one class in the file, otherwise there does need to be a class called Pipeline

--- operon/_cli/subcommands/__init__.py
+++ operon/_cli/subcommands/__init__.py
@@ -1,8 +1,13 @@
 import os
+import inspect
 
 from operon._util.home import get_operon_home, load_pipeline_file
 from operon._util.errors import MalformedPipelineError
 from operon.components import ParslPipeline
+
+FLATTEN = 0
+MODULE_NAME = 0
+MODULE_INSTANCE = 1
 
 
 class BaseSubcommand(object):
@@ -23,16 +28,44 @@ class BaseSubcommand(object):
         else:
             return None
 
+        # Get all classes in the pipeline file
+        classes_in_pipeline_mod = [
+            c for c in
+            inspect.getmembers(pipeline_mod, inspect.isclass)
+            if '__operon.pipeline' in str(i[MODULE_INSTANCE])
+        ]
+        # If there is only one class in the pipeline file, use that class
+        if len(classes_in_pipeline_mod) == 1:
+            pipeline_class = classes_in_pipeline_mod[FLATTEN][MODULE_INSTANCE]
+        # If there are multiple classes, attempt to find one called Pipeline
+        elif len(classes_in_pipeline_mod) > 1:
+            try:
+                pipeline_class = [
+                    c[MODULE_INSTANCE]
+                    for c in classes_in_pipeline_mod
+                    if c[MODULE_NAME] == 'Pipeline'
+                ][FLATTEN]
+            except:
+                # If Pipeline does not exist, send back None
+                raise MalformedPipelineError(
+                    'Pipeline file contained multiple classes, none of '
+                    'which were called \'Pipeline\'\n'
+                    'Try the form:\n\n'
+                    '\tclass Pipeline(ParslPipeline):\n')
+        else:
+            # If there are zero classes found, send back None
+            raise MalformedPipelineError('Pipeline file has no classes')
+
         # Return pipeline instance
         try:
             # Ensure Pipeline subclasses ParslPipeline
-            if not issubclass(pipeline_mod.Pipeline, ParslPipeline):
+            if not issubclass(pipeline_class, ParslPipeline):
                 raise MalformedPipelineError(
                     'Pipeline class does not subclass ParslPipeline\n'
                     'Try the form:\n\n'
                     '\tclass Pipeline(ParslPipeline):\n'
                 )
-            return pipeline_mod.Pipeline()
+            return pipeline_class()
         except AttributeError:
             # Ensure the pipeline file contains a class called Pipeline
             raise MalformedPipelineError(
